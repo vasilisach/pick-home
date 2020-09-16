e89ed71dc2fd4dfc315c0acb82e415076393094c
Vasylyna <chornei.v.v.31@gmail.com>
2020-09-16 13:38:31 +0300

    LEARN-476: Price section design improved

--- index.js
+++ index.js
@@ -35,21 +35,53 @@
 
   //main menu
   const menuButton = document.querySelector('.menu-icon');
-    const mainMenu = document.querySelector('.menu');
-    const navBar = document.querySelector('.main-page-header__wrapper');
+  const mainMenu = document.querySelector('.menu');
+  const navBar = document.querySelector('.main-page-header__wrapper');
 
 
-    if (menuButton) {
-      menuButton.addEventListener('click', clickMenuButton);
+  if (menuButton) {
+    menuButton.addEventListener('click', clickMenuButton);
+  }
+
+  function clickMenuButton(event) {
+    let element = document.querySelector('.menu-icon__button');
+    element.classList.toggle('menu-icon__button--toggle');
+    navBar.classList.toggle('main-page-header__wrapper--active');
+    if (mainMenu) {
+       mainMenu.classList.toggle('menu--active');
     }
+  }
 
-    function clickMenuButton(event) {
-      let element = document.querySelector('.menu-icon__button');
-      element.classList.toggle('menu-icon__button--toggle');
-      navBar.classList.toggle('main-page-header__wrapper--active');
-      if (mainMenu) {
-        mainMenu.classList.toggle('menu--active');
-      }
+  //price-table
+  const priceBlock = document.getElementById('price');
+  let sliderButtons, priceTable;
+
+  if (priceBlock) {
+    const sliderControls = priceBlock.querySelector('.slider-controls');
+    priceTable = priceBlock.querySelector('.price__table');
+
+    sliderButtons = Array.from(sliderControls.querySelectorAll('.slider-controls__button'));
+
+    if (sliderButtons) {
+      sliderControls.addEventListener('click', clickSliderControls);
     }
+  }
+
+  function findCurrentButton(element) {
+    return element.classList.contains('slider-controls__button--current');
+  }
+
+  function clickSliderControls(evt) {
+    let element = evt.target;
+
+    if (element.classList.contains('slider-controls__button')) {
+      let indexCurrentButton = sliderButtons.indexOf(sliderButtons.find(findCurrentButton));
+      let indexNextButton = sliderButtons.indexOf(element);
+
+      sliderButtons[indexCurrentButton].classList.remove('slider-controls__button--current');
+      sliderButtons[indexNextButton].classList.add('slider-controls__button--current');
+      priceTable.style.left = `${(150 - indexNextButton * 100)}%`;
+    }
+  }
     
 })()
