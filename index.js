28c2caa16d36b6ec4efb5f135cbdfe68de1bd616
Vasylyna <chornei.v.v.31@gmail.com>
2020-09-17 06:59:42 +0300

    LEARN-476: Switch slides improved

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 (function () {
-  //review slider
+  
   $(document).ready(function () {
     
-    if (document.body.clientWidth > 1023) {
-      $('#review-slides').slick({
-        dots: false,
-        infinite: true,
-      });
-    } else {
-      $('#review-slides').slick({
-        dots: true,
-        infinite: true,
-      });
+    //main menu
+    const menuButton = document.querySelector('.menu-icon');
+    const mainMenu = document.querySelector('.menu');
+    const navBar = document.querySelector('.main-page-header__wrapper');
+
+
+    if (menuButton) {
+      menuButton.addEventListener('click', clickMenuButton);
     }
-    
-    $('#price-slider').slick()
+
+    function clickMenuButton(event) {
+      let element = document.querySelector('.menu-icon__button');
+      element.classList.toggle('menu-icon__button--toggle');
+      navBar.classList.toggle('main-page-header__wrapper--active');
+      if (mainMenu) {
+        mainMenu.classList.toggle('menu--active');
+      }
+    }
+
+    //review slider
+    $('#review-slides').slick({
+      dots: false
+    });
 
     const prevBtn = document.querySelector('.slider-nav__slider-button--prev');
     const nextBtn = document.querySelector('.slider-nav__slider-button--next');
@@ -23,65 +33,56 @@
       prevBtn.addEventListener('click', clickPrevSlideBtn);
       nextBtn.addEventListener('click', clickNextSlideBtn);
     }
+   
+    function clickPrevSlideBtn(event) {
+      $("#review-slides").slick('slickPrev');
+    }
+    function clickNextSlideBtn(event) {
+      $("#review-slides").slick('slickNext');
+    }
 
+    const reviewSliderControl = document.querySelector('.slider-reviews__slider-controls')
+    if (reviewSliderControl) {
+      reviewSliderControl.addEventListener('click', reviewSliderButtonsClickHandler)
+    }
+    function reviewSliderButtonsClickHandler(event) {
+      let element = event.target;
+      let sliderButtons = Array.from(reviewSliderControl.querySelectorAll('.slider-controls__button'));
+      if (element.classList.contains('slider-controls__button')) {
+        switchSlide(sliderButtons, element)
+      }
+    }
+
+    //price table slider
+    const priceSliderControl = document.querySelector('.price__slider-controls');
+    if (priceSliderControl) {
+      priceSliderControl.addEventListener('click', clickPriceSliderControls);
+    }
+
+    function clickPriceSliderControls(evt) {
+      let element = evt.target;
+
+      if (element.classList.contains('slider-controls__button')) {
+        let sliderButtons = Array.from(priceSliderControl.querySelectorAll('.slider-controls__button'));
+        switchSlide(sliderButtons, element)
+      }
+    }
   })
-
-  function clickPrevSlideBtn(event) {
-    $("#review-slides").slick('slickPrev');
+  //slider functions
+  function findActiveSliderButton(element) {
+    return element.classList.contains('slider-controls__button--active');
   }
-  function clickNextSlideBtn(event) {
-    $("#review-slides").slick('slickNext');
-  }
+  function switchSlide(sliderButtons, element) {
+    let indexCurrentButton = sliderButtons.indexOf(sliderButtons.find(findActiveSliderButton));
+    let indexNextButton = sliderButtons.indexOf(element);
 
-  //main menu
-  const menuButton = document.querySelector('.menu-icon');
-  const mainMenu = document.querySelector('.menu');
-  const navBar = document.querySelector('.main-page-header__wrapper');
-
-
-  if (menuButton) {
-    menuButton.addEventListener('click', clickMenuButton);
-  }
-
-  function clickMenuButton(event) {
-    let element = document.querySelector('.menu-icon__button');
-    element.classList.toggle('menu-icon__button--toggle');
-    navBar.classList.toggle('main-page-header__wrapper--active');
-    if (mainMenu) {
-       mainMenu.classList.toggle('menu--active');
-    }
-  }
-
-  //price-table
-  const priceBlock = document.getElementById('price');
-  let sliderButtons, priceTable;
-
-  if (priceBlock) {
-    const sliderControls = priceBlock.querySelector('.slider-controls');
-    priceTable = priceBlock.querySelector('.price__table');
-
-    sliderButtons = Array.from(sliderControls.querySelectorAll('.slider-controls__button'));
-
-    if (sliderButtons) {
-      sliderControls.addEventListener('click', clickSliderControls);
-    }
-  }
-
-  function findCurrentButton(element) {
-    return element.classList.contains('slider-controls__button--current');
-  }
-
-  function clickSliderControls(evt) {
-    let element = evt.target;
-
-    if (element.classList.contains('slider-controls__button')) {
-      let indexCurrentButton = sliderButtons.indexOf(sliderButtons.find(findCurrentButton));
-      let indexNextButton = sliderButtons.indexOf(element);
-
-      sliderButtons[indexCurrentButton].classList.remove('slider-controls__button--current');
-      sliderButtons[indexNextButton].classList.add('slider-controls__button--current');
+    sliderButtons[indexCurrentButton].classList.remove('slider-controls__button--active');
+    sliderButtons[indexNextButton].classList.add('slider-controls__button--active');
+    if (element.parentElement.classList.contains('slider-reviews__slider-controls')) {
+      $("#review-slides").slick('slickGoTo', indexNextButton);
+    } else {
+      let priceTable = document.querySelector('.price__table');
       priceTable.style.left = `${(150 - indexNextButton * 100)}%`;
     }
   }
-    
 })()
